cccb9b96f58a2e6f674bfe917254f8682523f0bb
BeGuillaume <guillaume.bernos@gmail.com>
2018-11-15 12:15:22 +0800

    one chunk recovery

--- Project2/parity.py
+++ Project2/parity.py
@@ -21,6 +21,13 @@ def shift(x, n):
         s = s[1:] + s[0]
     return int(s, base=2)
 
+def unshift(x, n):
+    s = str(bin(x))[2:]
+    for i in range(CHUNK_SIZE - len(s)):
+        s = "0" + s
+    for i in range(n):
+        s =  s[-1] + s[:-1] 
+    return int(s, base=2)
 
 def calculate_Q(list_chunks):
     c = list_chunks[0]
@@ -30,16 +37,36 @@ def calculate_Q(list_chunks):
     return c
         
 
+def recover_one_chunk_with_P(remaining_chunks, P_chunk):
+    c = P_chunk
+    print("c",c, type(c))
+    for x in remaining_chunks:
+        c = c ^ x 
+
+    return c
+
+def recover_one_chunk_with_Q(all_disk_chunks, Q_chunk, missing_chunk_index):
+    c = Q_chunk
+    for i in range(0,len(all_disk_chunks)):
+        if i == missing_chunk_index:
+            continue
+
+        c = c ^ shift(all_disk_chunks[i], i)
+
+    return unshift(c, missing_chunk_index)
 
 
 
 if (DEBUG):
-    TEST_LIST = [0b11111111,0b01010101]
+    TEST_LIST = [0b11111111,0b11010101,0b11110011]
+    print(TEST_LIST)
 
-    print(bin(calculate_P(TEST_LIST)))
-    print(bin(shift(0b00010011, 1)))
-    print(bin(shift(0b00010011, 2)))
+    P = (calculate_P(TEST_LIST))
+    print("P",P, type(P))
+    Q = (calculate_Q(TEST_LIST))
+    print("Q",Q, type(Q))
 
-    print(bin(calculate_Q(TEST_LIST)))
+    print("Recovered:",recover_one_chunk_with_P(TEST_LIST[:-1], P))
+    print("Recovered:",recover_one_chunk_with_Q(TEST_LIST, Q, 2))
 
 
